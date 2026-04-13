327d421b6c47b8859b1e31cc457af942a546f8d3
zdward00 <zdward00@gmail.com>
2026-04-13 13:39:58 -0400

    more dark mode fixes

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { cn } from "@/lib/utils"
 
 export const ThemeToggle = () => {
-    const [isDarkMode, setIsDarkMode] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(true);
     
     useEffect(() => {
         const storedTheme = localStorage.getItem("theme");
